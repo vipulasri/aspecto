053ef44d0d504350dd4d0ef0374022daf7010821
vipulasri <vipulasri.2007@gmail.com>
2024-11-23 23:51:49 -0500

    add: basic AspectoGrid

--- aspecto/src/main/java/com/vipulasri/aspecto/AspectoGrid.kt
+++ aspecto/src/main/java/com/vipulasri/aspecto/AspectoGrid.kt
@@ -1,5 +1,28 @@
 package com.vipulasri.aspecto
 
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
 /**
  * Created by Vipul Asri on 23/11/24.
  */
+
+@Composable
+fun AspectoGrid(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    maxRowHeight: Dp = AspectoRowCalculator.DEFAULT_MAX_ROW_HEIGHT.dp,
+    itemPadding: PaddingValues = PaddingValues(0.dp),
+    content: LazyAspectoLayoutScope.() -> Unit
+) {
+    BoxWithConstraints(
+        modifier = modifier.fillMaxWidth()
+    ) {
+
+    }
+}
