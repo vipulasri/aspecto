fdf9967f00992d75835adbc294e835223d7d4d7e
vipulasri <vipulasri.2007@gmail.com>
2025-01-05 00:07:34 -0500

    update: remove commented code

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -87,22 +87,3 @@ android {
 dependencies {
     debugImplementation(compose.uiTooling)
 }
-
-//dependencies {
-//    implementation(libs.androidx.core.ktx)
-//    implementation(libs.androidx.lifecycle.runtime.ktx)
-//    implementation(libs.androidx.activity.compose)
-//    implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.bundles.compose)
-//    implementation(libs.androidx.compose.material3)
-//
-//    debugImplementation(libs.bundles.compose.debug)
-//
-//    implementation(libs.coil)
-//
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
-//    androidTestImplementation(platform(libs.androidx.compose.bom))
-//    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-//}
