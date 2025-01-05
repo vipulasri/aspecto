d46795704bbfb6e44f9da998c9e201111ecfd9fa
vipulasri <vipulasri.2007@gmail.com>
2025-01-04 21:03:49 -0500

    add: maven publishing dependency and configuration

--- aspecto/build.gradle.kts
+++ aspecto/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.mavenPublish)
 }
 
 kotlin {
